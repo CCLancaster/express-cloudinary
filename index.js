7ead0b316051678e6dd815a160d7a5bd80efd7a1
CCLancaster <connieclancaster@gmail.com>
2020-02-18 11:52:35 -0800

    added intial post route

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var cloudinary = require('cloudinary');
+var multer = require('multer');
+var upload = multer({ dest: './uploads' });
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
@@ -9,4 +13,7 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.post('/', upload.single('inputFile'), function(req, res) {
+  res.send(req.file);
+})
 app.listen(3000);
