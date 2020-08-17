97884bf4194dff08f5f81612e1da13c3d6551469
CCLancaster <connieclancaster@gmail.com>
2020-08-17 09:31:00 -0700

    played with code during presentation to new cohort

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var ejsLayouts = require('express-ejs-layouts');
 var app = express();
 var cloudinary = require('cloudinary');
 var multer = require('multer');
-var upload = multer({ dest: './uploads' });
+var upload = multer({ dest: './uploadspttwo' });
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
@@ -16,10 +16,11 @@ app.get('/', function(req, res) {
 app.post('/', upload.single('inputFile'), function(req, res) {
   cloudinary.uploader.upload(req.file.path, function(result) {
     var cloudID = result.public_id;
-    var imageLink = `https://res.cloudinary.com/cclancaster/image/upload/e_sepia:80,q_auto:good/v1582053889/${cloudID}.jpg`
+    var imageLink = `https://res.cloudinary.com/cclancaster/image/upload/c_fit,e_oil_paint:65,h_400,w_400/v1582053889/${cloudID}.jpg`
     res.render('result', { image: imageLink });
   })
 });
+
 
 app.listen(3000);
 
